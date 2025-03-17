94c8aa7161373e9cecd82319d652c806b55e204d
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-17 16:04:35 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+try:
+  contador = 0
+  for contador in range(3,101):
+    if (contador % 3 == 0):
+      print(f" {contador}")
+except Exception as ERRO_EXCEÇÃO:
+  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCEÇÃO}")
