ca27ec72886989f7e132d960df23567abefdbcd0
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-20 15:12:14 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+try:
+  num = 0
+  somaPar = 0
+  somaImpar = 0
+  for num in range(10, 100):
+    if (num % 2 == 0):
+      somaPar += num
+    else:
+      somaImpar += num
+  print(f"A soma dos números Pares é: {somaPar}")
+  print(f"A soma dos números Ímpares é: {somaImpar}")
+except Exception as ERRO_EXCECAO:
+  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
