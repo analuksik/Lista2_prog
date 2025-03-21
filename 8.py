fa7765aabef75d0c8b12534944ea1210a6e97bac
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-21 14:50:18 -0300

    Create 8.py
    TESTAR

---
+++ 8.py
@@ -0,0 +1,34 @@
+
+try:
+
+  op1 = 0
+  op2 = 0
+  op3 = 0
+  cliente = 0
+
+  while True:
+
+    print("Qual sabor de mix você mais gostou?")
+    opcao = int(input("Opções: 1: Mix 1, 2: Mix 2 ou 3: Mix 3"))
+    print("Digite 0 para sair do progama")
+
+    cliente += 1
+
+    if opcao == 1:
+      op1 += 1
+    elif opcao == 2:
+     op2 += 1
+    elif opcao == 3:
+     op3 += 1
+    elif opcao == 0:
+     break
+  porc1 = (op1 / cliente) * 100
+  porc2 = (op2 / cliente) * 100
+  porc3 = (op3 / cliente) * 100
+
+  print(f"A porcentagem de clientes que preferem o mix 1 é de: {porc1:.2f}%")
+  print(f"Mix 2: {porc2:.2f}%")
+  print(f"Mix 3:{porc3:.2f}%")
+  
+except Exception as ERRO_EXCECAO:
+  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
