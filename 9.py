bc2b092530399499f36362c796604c766878f1a2
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-22 10:37:12 -0300

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,14 @@
+try:
+  cont = 0
+  soma = 0
+  n = 10
+  while True:
+    cont += 1
+    n = int(input(f"Digite o {cont}º valor para n: "))
+    if n == 0:
+      break
+    if n >= 10 and n <= 90 and n % 5 == 2:
+      soma += n  
+  print(f"A soma dos números lidos é: {soma}")
+except Exception as ERRO_EXCECAO:
+  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
