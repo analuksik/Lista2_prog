5154f6f5a53705bef49ab291e94423f7f49d144e
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-25 11:56:36 -0300

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,17 +1,16 @@
-try:
-  soma = 0
-  q = 0
-  while True:
+soma = 0
+q = 0
+while True:
+  try:
     temp = float(input(f'Digite a temperatura em graus Celsius (°C): '))
     if temp < 28:
       break
     else:
       soma += temp
       q += 1
-  media = soma / q
-  print(f'A média das temperaturas é de: {media}°C')
 except Exception as ERRO_EXCECAO:
   print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')
-
+media = soma / q
+print(f'A média das temperaturas é de: {media}°C')
 
     
