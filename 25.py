680ae7e1248331fb7af9f813bd7419de74359e71
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-25 19:03:42 -0300

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,29 @@
+import math
+
+paciente = 0
+for i in range(0, 5):
+
+  try:
+
+    idade = int(input('Digite a idade do paciente: '))
+
+    if idade < 1:
+      print('A idade deve ser maior do que zero. ')
+      continue
+
+    massa = float(input('Digite a massa do paciente (kg): '))
+    diab = str(input('O paciente é diabético? (S/N) '))
+
+    if diab == 'S':
+      taxa = (math.sqrt(massa)) / (0.93 * idade)
+
+    elif diab == 'N':
+      taxa = (math.sqrt(0.98 * massa)) / (1.08 * idade)
+    else:
+      print('Resposta inválida!')
+      print('Digite "S" para sim ou "N" para não')
+
+    print(f'A taxa de glicose do paciente é de: {taxa:.2f}')
+
+  except Exception as ERRO_EXCECAO:
+    print(f'ERRO DE EXCEÇÃO: {ERRO_EXCECAO}')  
