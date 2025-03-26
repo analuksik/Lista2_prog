11a292b08e8ecb3867d2c0b7444e09adfd7dc5b6
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-26 15:32:14 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,16 +1,22 @@
 try:
+
   contador = 0
   soma = 0
   qtd = 0
+
   print(f"Os múltiplos de 11 no intervalo [200, 100] são: ")
+
   for contador in range(200, 99, -1):
     if (contador % 11 == 0):
       print(f"{contador}")
       soma += contador
       qtd += 1
+
   if qtd > 0:
+    
     media = soma / qtd
     print(f"A soma dos múltiplos de 11 é: {soma}")
     print(f"A média dos múltiplos de 11 é: {media:.2f}")  
+
 except Exception as ERRO_EXCECAO:
   print(f"ERRO DE EXEÇÃO: {ERRO_EXCECAO}")
