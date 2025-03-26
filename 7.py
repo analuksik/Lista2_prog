0bc3a96a9ccab675bbe507ea24c0c9db38efdbf8
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-26 20:07:53 -0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,14 +1,10 @@
-try:
-  i = 0
-  cont = 0
-  n = 0
+i = 0
+cont = 0
+n = 0
 
-  for i in range(9, 98):
-    if (i % 2 != 0) and (i % 3 == 0) and (i % 5 != 0):
-      n += 1
-      print(f"{n}° número: {i}")
-      cont += i
+for i in range(9, 98):
+  if (i % 2 != 0) and (i % 3 == 0) and (i % 5 != 0):
+    n += 1
+    print(f"{n}° número: {i}")
+    cont += i
   print(f"A soma dos números é: {cont}")
-except Exception as ERRO_EXCECAO:
-  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
-
