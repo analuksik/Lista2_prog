8971bc591a56be8c0931e70ad72362a512984cba
Ana Clara Luksik <167776937+analuksik@users.noreply.github.com>
2025-03-26 20:09:36 -0300

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,12 +1,11 @@
+op1 = 0
+op2 = 0
+op3 = 0
+cliente = 0
 
-try:
+while True:
 
-  op1 = 0
-  op2 = 0
-  op3 = 0
-  cliente = 0
-
-  while True:
+  try:
 
     print("Qual sabor de mix você mais gostou?")
     opcao = int(input("Opções: 1: Mix 1, 2: Mix 2 ou 3: Mix 3"))
@@ -21,15 +20,15 @@ try:
     elif opcao == 0:
      break
     cliente += 1
-    
-  if cliente > 0:  
+  
+  except Exception as ERRO_EXCECAO:
+    print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
+
+if cliente > 0:  
   porc1 = (op1 / cliente) * 100
   porc2 = (op2 / cliente) * 100
   porc3 = (op3 / cliente) * 100
 
-  print(f"A porcentagem de clientes que preferem o mix 1 é de: {porc1:.2f}%")
-  print(f"Mix 2: {porc2:.2f}%")
-  print(f"Mix 3:{porc3:.2f}%")
-  
-except Exception as ERRO_EXCECAO:
-  print(f"ERRO DE EXCEÇÃO: {ERRO_EXCECAO}")
+print(f"A porcentagem de clientes que preferem o mix 1 é de: {porc1:.2f}%")
+print(f"Mix 2: {porc2:.2f}%")
+print(f"Mix 3:{porc3:.2f}%")
